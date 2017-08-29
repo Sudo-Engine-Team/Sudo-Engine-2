366a48802b7bd26019b2ebdeb59779b330b1e000
Timothy Gibbons <tjgibbons10@gmail.com>
2017-08-29 09:46:41 -0500

    Made starting an application more convenient.
    Renamed GameLauncher to Application.
    Set the screen in the application;
    Set the tile in the app.
    Set the Width, and height in the app.

---
+++ src/site/root3287/sudo2/display/Application.java
@@ -0,0 +1,25 @@
+package site.root3287.sudo2.display;
+
+import org.lwjgl.glfw.GLFW;
+
+public class Application{
+	public Application(String title) {
+		DisplayManager.TITLE = title;
+	}
+	public Application(String title, float width, float height) {
+		DisplayManager.TITLE = title;
+		DisplayManager.WIDTH = width;
+		DisplayManager.HEIGHT = height;
+	}
+	public Application setScreen(Screen s){
+		DisplayManager.setScreen(s);
+		return this;
+	}
+	public void run(){
+		DisplayManager.init();
+		while(!GLFW.glfwWindowShouldClose(DisplayManager.WINDOW)){
+			DisplayManager.loop();
+		}
+		DisplayManager.dispose();
+	}
+}
