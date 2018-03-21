cc18131600fa3cbec1e3602ff8ce490853ae6ff6
Timothy Gibbons <tjgibbons10@gmail.com>
2018-03-21 13:52:59 -0500

    Started Making API to homebase
    created API class...

---
+++ src/site/root3287/sudo2/api/API.java
@@ -0,0 +1,14 @@
+package site.root3287.sudo2.api;
+
+import java.net.URL;
+
+public class API {
+	private static final String HOST = "https://api.root3287.site/";
+	private String publicKey;
+	private URL host;
+	
+	public API(String appKey) {
+		
+	}
+
+}
