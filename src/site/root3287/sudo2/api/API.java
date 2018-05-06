462687acb250dba1f76e7184ec13aa75d7fa006f
Timothy Gibbons <tjgibbons10@gmail.com>
2018-05-05 22:21:29 -0500

    Fix eclipse warnings

--- src/site/root3287/sudo2/api/API.java
+++ src/site/root3287/sudo2/api/API.java
@@ -3,7 +3,7 @@ package site.root3287.sudo2.api;
 import java.net.URL;
 
 public class API {
-	private static final String HOST = "https://api.root3287.site/";
+	public static final String HOST = "https://api.root3287.site/";
 	private String publicKey;
 	private URL host;
 	
--- src/site/root3287/sudo2/engine/Renderable3D.java
+++ src/site/root3287/sudo2/engine/Renderable3D.java
@@ -13,5 +13,29 @@ public class Renderable3D {
 	public Renderable3D() {
 		// TODO Auto-generated constructor stub
 	}
+	public Vector3f getPosition() {
+		return position;
+	}
+	public void setPosition(Vector3f position) {
+		this.position = position;
+	}
+	public Vector3f getScale() {
+		return scale;
+	}
+	public void setScale(Vector3f scale) {
+		this.scale = scale;
+	}
+	public VAO getModel() {
+		return vao;
+	}
+	public void setModel(VAO vao) {
+		this.vao = vao;
+	}
+	public Texture getTexture() {
+		return texture;
+	}
+	public void setTexture(Texture texture) {
+		this.texture = texture;
+	}
 
 }
